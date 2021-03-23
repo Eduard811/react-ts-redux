2a1d1ace56644905ced5789a8bc850fb6cea97e8
Eduard811 <eduard.albertyan1@mail.ru>
2021-03-23 18:34:09 +0300

    create hook useActions

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,28 @@
+import { useEffect } from 'react'
+import { useActions } from '../hooks/useActions'
 import { useTypedSelector } from '../hooks/useTypedSelector'
+import { fetchUsers } from '../store/action-creators/user'
 
 const UserList: React.FC = () => {
 
     const {users, isLoading, error} = useTypedSelector(state => state.user)
+    const {fetchUsers} = useActions()
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    if (isLoading) {
+        return <h1>Идет загрузка...</h1>
+    }
+
+    if (error) {
+        return <h1>{error}</h1>
+    }
 
     return (
         <div>
-
+            {users.map(user => <div key={user.id}>{user.name}</div>)}
         </div>
     )
 }
