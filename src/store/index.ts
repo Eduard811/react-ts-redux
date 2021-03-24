1fecf1c46b3042945b4ff25a230b1003515935f1
Eduard811 <eduard.albertyan1@mail.ru>
2021-03-24 10:05:28 +0300

    add redux-devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,8 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { rootReducer } from './reducers'
 
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const composeEnhancers =
+  (window && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
